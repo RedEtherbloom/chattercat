95f536279adf2964aa302cd1836870f2e459fa70
RedEtherbloom <etherbloom@mailbox.org>
2025-09-12 20:36:14 +0200

    pipewire: basic connection

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,33 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use pipewire::context::Context as PipewireContext;
+use pipewire::core::Core as PipewireCore;
+use pipewire::main_loop::MainLoop;
+use pipewire::registry::Registry as PipewireRegistry;
+
+#[derive(Debug, Clone)]
+struct Audio {
+    main_loop: MainLoop,
+    audio_context: PipewireContext,
+    pipewire_core: PipewireCore,
+    pipewire_registry: PipewireRegistry,
+}
+
+impl Audio {
+    pub fn init() -> Result<Audio, Box<dyn std::error::Error>> {
+        let main_loop = MainLoop::new(None)?;
+        let audio_context = PipewireContext::new(&main_loop)?;
+        let pipewire_core = audio_context.connect(None)?;
+        let pipewire_registry = pipewire_core.get_registry()?;
+
+        Audio {
+            main_loop,
+            audio_context,
+            pipewire_core,
+            pipewire_registry,
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
